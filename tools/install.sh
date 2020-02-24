6c71556c6ed99101cdb3997feb8611f8c22c3d3c
sunquan <sunquana@gmail.com>
2020-02-24 11:21:50 +0800

    add ss_port.sh

---
+++ tools/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dir=`pwd`
+dst="/usr/local/bin"
+
+echo "install ss,myunzip,get_ss_port into: "$dst
+#ln -s $dir/ss.sh $dst/ss
+#ln -s $dir/myunzip.py $dst/myunzip.py
+#ln -s $dir/get_ss_port.sh $dst/get_ss_port.sh
+
