744dccfa98e8ded81abeef5aca89673284be44fe
sunquan <sunquana@gmail.com>
2020-06-11 23:06:06 +0800

    macos package sync.

--- tools/install_software_macos.sh
+++ tools/install_software_macos.sh
@@ -1,7 +1,69 @@
-# sunquan update 202-04-04 22:51
+#!/bin/bash
 
-brew install iftop p7zip tmux
+# sunquan update at 202-06-11 22:59
+basic_tools=(
+    ascii
+    dos2unix
+    gdb
+    gdbm
+    gettext
+    gflags
+    git
+    htop
+    iftop
+    iperf
+    nasm
+    nghttp2
+    node
+    p7zip
+    parallel
+    python
+    readline
+    tmux
+    tree
+    unrar
+    watch
+    wget
+)
 
-# aria2gui 下载工具
-brew cask install aria2gui
+macos_packages=(
+    "${basic_tools[@]}"
+    autoconf
+    automake
+    bazel
+    cmake
+    boost
+    bwm-ng
+    iproute2mac
+    c-ares
+    elixir
+    erlang
+    findutils
+    go     # google
+    grpc
+    protobuf
+    graphviz
+    jemalloc
+    libssh
+    libtool
+    libuv
+    maven
+    nginx
+    openssl
+    redis
+    spdlog
+    sqlite
+    tinyxml2 # config
+    yaml-cpp
+    zeromq
+)
+
+cask_gui_tools=(
+    aria2gui  # download file 
+    copyq     # clipboard enhance
+)
+
+brew install "${macos_packages[@]}" bochs hugo
+
+brew cask install  "${cask_gui_tools[@]}"
 
