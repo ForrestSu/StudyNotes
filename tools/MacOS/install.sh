fb229ce2f76f933c22662faefc9f3c6a12f6b0ff
sunquan <sunquana@gmail.com>
2022-03-10 20:30:01 +0800

    feat(alias): add cloc_by_user,rm_go_test

--- tools/MacOS/install.sh
+++ tools/MacOS/install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# 覆盖zsh builtin 的 time 关键字
-echo 'alias time="/usr/bin/time -h"' >> ~/.zshrc
-echo 'alias ss="lsof -nP -i4TCP | grep LISTEN"' >> ~/.zshrc
-echo 'alias sed=gsed' >> ~/.zshrc
+curdir=`pwd`
+echo "append to ~/.zshrc: "
+cat ${curdir}/zshrc >> ~/.zshrc
 
-dir=`pwd`
-dst="/usr/local/bin"
-
-echo "install <myunzip> into: "$dst
-ln -s $dir/myunzip.py $dst/myunzip.py
+dest="/usr/local/bin"
+echo "install <myunzip.py> into: "${dest}
+ln -s ${curdir}/myunzip.py ${dest}/myunzip.py
